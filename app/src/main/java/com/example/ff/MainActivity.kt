36808b383a4580d7098b3128418c0a29bd3e936b
thqmasinsomnia <tsmith142@saddleback.edu>
2021-04-09 16:38:39 -0700

    slightly fixed UPC loading issue but not happy with it
    added the LOAD UPC option to avoid having to open the scanner again to load the name of the item

--- app/src/main/java/com/example/ff/MainActivity.kt
+++ app/src/main/java/com/example/ff/MainActivity.kt
@@ -2,18 +2,22 @@ package com.example.ff
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.internal.GsonBuildConfig
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupActionBarWithNavController(findNavController(R.id.fragment))
-
+        fetchJson()
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -22,4 +26,35 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    fun fetchJson() {
+        println("************ATEMPTING JSON FETCH**********************")
+        val url = "http://whydoifeellikeafailure.com/upcs.json"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val body = response?.body?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+
+
+              val upcList = gson.fromJson(body, UPCList::class.java)
+
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("failed request")
+            }
+
+        })
+
+    }
+
+
 }
+
+
+class UPCList(val upcs: List<UPC>)
+
+class UPC(val item_name: String?, val upc_code: String?)
--- app/src/main/java/com/example/ff/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/ff/fragments/add/AddFragment.kt
@@ -1,42 +1,50 @@
 package com.example.ff.fragments.add
-
-
-
-
-
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.inflate
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.findFragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.ff.R
+import com.example.ff.UPCList
 import com.example.ff.model.FridgeItem
 import com.example.ff.viewmodel.FridgeViewModel
+import com.google.gson.GsonBuilder
 import com.google.zxing.integration.android.IntentIntegrator
+import okhttp3.*
+import org.w3c.dom.Text
+import java.io.IOException
 
 
 class AddFragment : Fragment() {
 
     private lateinit var mFridgeViewModel: FridgeViewModel
 
-    var upc = "HELLO"
+    var upc = ""
+    var itemname = "Item Name"
+
+    var foundItem = false
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_add, container, false)
 
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+        view?.findViewById<EditText>(R.id.itemName)?.setText(itemname)
         mFridgeViewModel = ViewModelProvider(this).get(FridgeViewModel::class.java)
 
         view.findViewById<Button>(R.id.addButton).setOnClickListener(){
@@ -44,31 +52,50 @@ class AddFragment : Fragment() {
         }
 
 
-        view.findViewById<Button>(R.id.scanButton).setOnClickListener(){
-             val scanner = IntentIntegrator.forSupportFragment(this)
+        view.findViewById<Button>(R.id.scanButton).setOnClickListener() {
+            val scanner = IntentIntegrator.forSupportFragment(this)
             scanner.initiateScan()
-            view.findViewById<TextView>(R.id.upc).text = upc
+        }
 
+        view.findViewById<Button>(R.id.loaditem).setOnClickListener(){
+            println("stupid fix")
+            view?.findViewById<EditText>(R.id.itemName)?.setText(itemname)
 
         }
 
         return view
-
     }
+
+
      override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+         println("SCANNER CALLED")
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         if (result != null) {
             if (result.contents == null) {
                 Toast.makeText(requireContext(), "Cancelled", Toast.LENGTH_LONG).show()
             } else {
                 Toast.makeText(requireContext(), "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
-                upc = result.contents
+                upc = result.contents.toString()
+
 
 
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
+
+
+         }
+
+
+
+    override fun onStart() {
+
+        println("IM BACCCCCCCCCCCCCCCCCCCCCCK")
+
+        super.onStart()
+        fetchUPC(upc)
+
     }
 
 
@@ -98,6 +125,46 @@ class AddFragment : Fragment() {
 
     }
 
+    fun fetchUPC(passedUPC: String) {
+        println("************ATEMPTING JSON FETCH**********************")
+        val url = "http://whydoifeellikeafailure.com/upcs.json"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+            @SuppressLint("ShowToast")
+            override fun onResponse(call: Call, response: Response) {
+                val body = response?.body?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+                var upcList = gson.fromJson(body, UPCList::class.java)
+
+
+                for (item in upcList.upcs) {
+                    if (passedUPC == item.upc_code) {
+                        itemname = item.item_name.toString()
+                        println(itemname)
+                        println(item.item_name.toString())
+                    }
+                }
+
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("failed request")
+            }
+
+        })
+
+    }
+
+
+
+
+
 
 
 }
+
+
+
